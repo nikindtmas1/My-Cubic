d657ebde97bb638a9e5b36bde101a9b6a371be58
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 21:53:40 +0300

    Update routes.js
    Added logic create page

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,16 @@
 // TODO: Require Controllers...
+//const express = require('express');
+//const router = express.Router();
 const homePage = require('../controllers/homeController');
+const cubicsController = require('../controllers/cubicsController');
+//const errorPage = require('../views/404.hbs');
 
 module.exports = (app) => {
     // TODO...
 
     app.use('/', homePage);
+    app.use('/cubics', cubicsController)
+    // router.get('*', (req, res) => {
+    //     res.render('404');
+    // });
 };
