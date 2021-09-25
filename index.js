90bc89663843e6d660f3f43557db438133e189aa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 19:25:29 +0300

    Update index.js
    Set routes

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 //const port = 5000;
 const configPort = require('./config/config');
+const routes = require('./config/routes');
 
 const configExpress = require('./config/express');
 configExpress(app);
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    res.render('index', {layout: false});
-    //res.send('<h1>Hello From Express</h1>')
-})
+//     res.render('index', {layout: false});
+//     //res.send('<h1>Hello From Express</h1>')
+// })
+
+routes(app)
 
 app.listen(configPort.development.PORT, ()=> console.log(`Server listening on port: ${configPort.development.PORT}`))
