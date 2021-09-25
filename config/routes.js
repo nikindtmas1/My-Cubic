f3c56145e541c3a452e84778346f9ccd8411d3d6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 22:00:02 +0300

    Update routes.js
    Router redirect '/cubics

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 // TODO: Require Controllers...
-//const express = require('express');
-//const router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
 const homePage = require('../controllers/homeController');
 const cubicsController = require('../controllers/cubicsController');
 //const errorPage = require('../views/404.hbs');
@@ -10,7 +12,7 @@ module.exports = (app) => {
 
     app.use('/', homePage);
     app.use('/cubics', cubicsController)
-    // router.get('*', (req, res) => {
-    //     res.render('404');
-    // });
+    router.get('*', (req, res) => {
+        res.render('404', {layout: false});
+    });
 };
