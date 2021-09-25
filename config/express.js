5728dedf15b7bbe897d519cf900a35d783ff91a2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 18:49:26 +0300

    Update express.js
    Config handlebars

--- config/express.js
+++ config/express.js
@@ -6,6 +6,11 @@ module.exports = (app) => {
     
     //TODO: Setup the view engine
 
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
+    app.set('view engine', 'hbs');
+
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
