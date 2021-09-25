c7e1530608b8de2010cb313721cc7c95bb35386f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 22:00:22 +0300

    Update cubicsController.js
    Create cubictController

--- controllers/cubicsController.js
+++ controllers/cubicsController.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('index', {layout: false});
+});
+
 router.get('/create', (req, res) => {
     res.render('create', {layout: false});
 });
