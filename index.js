70ee966cfe9021dc5ad48842cd1f81d4d78be100
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-25 18:49:44 +0300

    Update index.js
    Require handlebars

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const configExpress = require('./config/express');
+configExpress(app);
+
 app.get('/', (req, res) => {
 
-    res.send('<h1>Hello From Express</h1>')
+    res.render('index', {layout: false});
+    //res.send('<h1>Hello From Express</h1>')
 })
 
 app.listen(port, ()=> console.log(`Server listening on port: ${port}`))
