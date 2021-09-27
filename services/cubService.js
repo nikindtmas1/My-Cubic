2e5067034759228f9be1f3c3d603917c21b47479
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-27 12:23:27 +0300

    Added logic data base

--- services/cubService.js
+++ services/cubService.js
@@ -2,10 +2,18 @@ const Cube = require('../models/Cube');
 const dbCub = require('../config/database.json');
 const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid');
 
-const create = (name, description, imageUrl, difficulty) => {
+function create(data){
     console.log(dbCub);
-    let cube = new Cube(name, description, imageUrl, difficulty);
+    let cube = new Cube(
+      
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficulty
+        );
+
     dbCub.push(cube);
     let json = JSON.stringify(dbCub, '', 2);
 
