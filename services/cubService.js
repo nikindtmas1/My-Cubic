9b9659a0c681360ef17d14c65872dd34bf1f0d02
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-27 11:58:24 +0300

    Added logic

--- services/cubService.js
+++ services/cubService.js
@@ -1,10 +1,20 @@
 const Cube = require('../models/Cube');
 const dbCub = require('../config/database.json');
+const fs = require('fs');
 
 const create = (name, description, imageUrl, difficulty) => {
-
+    console.log(dbCub);
     let cube = new Cube(name, description, imageUrl, difficulty);
     dbCub.push(cube);
+    let json = JSON.stringify(dbCub);
+
+    fs.writeFile(path.join(__dirname, '/../config/database.json'), json, (err) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
+    console.log(dbCub);
 };
 
 const cubeService = {
