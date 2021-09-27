ed290498845b86fd2a2674fa54fb9bfdbef382c4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-09-27 12:02:58 +0300

    Added logic for save data in data base

--- services/cubService.js
+++ services/cubService.js
@@ -1,12 +1,13 @@
 const Cube = require('../models/Cube');
 const dbCub = require('../config/database.json');
 const fs = require('fs');
+const path = require('path');
 
 const create = (name, description, imageUrl, difficulty) => {
     console.log(dbCub);
     let cube = new Cube(name, description, imageUrl, difficulty);
     dbCub.push(cube);
-    let json = JSON.stringify(dbCub);
+    let json = JSON.stringify(dbCub, '', 2);
 
     fs.writeFile(path.join(__dirname, '/../config/database.json'), json, (err) => {
         if(err){
